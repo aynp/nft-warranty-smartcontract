4b972d06a07027c2f8ab75ed8730c117546897e2
aynp <aryan.pathania2003@gmail.com>
2022-07-16 05:29:55 +0530

    Added Minting Functionality

--- contracts/WarrentyNFT.sol
+++ contracts/WarrentyNFT.sol
@@ -9,6 +9,23 @@ import '@openzeppelin/contracts/access/Ownable.sol';
 contract WarrentyNFT is ERC721, ERC721URIStorage, ERC721Burnable, Ownable {
   constructor() ERC721('Warrenty', 'W') {}
 
+  /**
+   * @notice mints a new token for the given buyer
+   * @dev serialNo of product is used as token id
+   * @param buyer wallet address of the buyer
+   * @param serialNo serial number of the product
+   * @param dataURI URI of the product
+   */
+
+  function mintWarrentyNFT(
+    address buyer,
+    uint256 serialNo,
+    string memory dataURI
+  ) public {
+    _mint(buyer, serialNo);
+    _setTokenURI(serialNo, dataURI);
+  }
+
   // The following functions are overrides required by Solidity.
 
   function _burn(uint256 tokenId) internal override(ERC721, ERC721URIStorage) {
