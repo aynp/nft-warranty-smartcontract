acf014f431ebfde807aa2081524f6e228e9a445b
aynp <aryan.pathania2003@gmail.com>
2022-07-16 05:09:36 +0530

    Made WarrentyNFT an ERC721 Smart Contract

--- contracts/WarrentyNFT.sol
+++ contracts/WarrentyNFT.sol
@@ -1,6 +1,26 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.9;
 
-contract WarrentyNFT {
-  constructor() {}
+import '@openzeppelin/contracts/token/ERC721/ERC721.sol';
+import '@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol';
+import '@openzeppelin/contracts/token/ERC721/extensions/ERC721Burnable.sol';
+import '@openzeppelin/contracts/access/Ownable.sol';
+
+contract WarrentyNFT is ERC721, ERC721URIStorage, ERC721Burnable, Ownable {
+  constructor() ERC721('Warrenty', 'W') {}
+
+  // The following functions are overrides required by Solidity.
+
+  function _burn(uint256 tokenId) internal override(ERC721, ERC721URIStorage) {
+    super._burn(tokenId);
+  }
+
+  function tokenURI(uint256 tokenId)
+    public
+    view
+    override(ERC721, ERC721URIStorage)
+    returns (string memory)
+  {
+    return super.tokenURI(tokenId);
+  }
 }
